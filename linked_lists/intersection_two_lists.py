957612b7bbc7343a3d54aada700d6aa6b8edf45b
MercyNJ <mercinjuguna@gmail.com>
2024-05-02 12:22:31 +0000

    Find intersection node in two linkedlists

--- linked_lists/intersection_two_lists.py
+++ linked_lists/intersection_two_lists.py
@@ -30,6 +30,29 @@ class Solution:
                 currentB = currentB.next
         return currentA
 
+#Different approach
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        currentA = headA
+        currentB = headB
+
+        visited = set()
+        while currentA:
+            visited.add(currentA)
+            currentA = currentA.next
+
+        while currentB:
+            if currentB in visited:
+                return currentB
+            currentB = currentB.next
+        return None
+
+
 # geeks for geeks implementation
 # utilzes set
 class ListNode:
