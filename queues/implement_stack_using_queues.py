274e428038126a4fe7e3a1b8bfcaa3fe752b1db4
MercyNJ <mercinjuguna@gmail.com>
2024-05-16 13:13:58 +0000

    Implementing a stack using two queues

--- queues/implement_stack_using_queues.py
+++ queues/implement_stack_using_queues.py
@@ -16,4 +16,51 @@ Notes:
     You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
     Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
 """
+class MyStack:
+    """
+    Stack implementation using two queues.
+    """
+    def __init__(self):
+        """
+        Initialization.
+        """
+        self.queue1 = []
+        self.queue2 = []
 
+    def push(self, x: int) -> None:
+        """ Add elememt to stack."""
+        self.queue2.append(x)
+        while self.queue1:
+            self.queue2.append(self.queue1.pop(0))
+        self.queue1, self.queue2 = self.queue2, self.queue1
+
+    def pop(self) -> int:
+        """Removes and returns top element"""
+        return self.queue1.pop(0)
+
+    def top(self) -> int:
+        """Returns top elem without removing it."""
+        return self.queue1[0]
+
+    def empty(self) -> bool:
+        return not self.queue1
+
+def test_my_stack():
+    stack = MyStack()
+
+    stack.push(1)
+    stack.push(2)
+
+    assert stack.top() == 2
+
+    assert stack.pop() == 2
+
+    assert not stack.empty()
+
+    assert stack.pop() == 1
+
+    assert stack.empty()
+
+    print("All test cases passed!")
+
+test_my_stack()
