e676c58cb0703cf03feaddf9a55d2ef588fc0f5f
MercyNJ <mercinjuguna@gmail.com>
2024-05-17 09:07:58 +0000

    Implement a stack using queue

--- queues/implement_stack_using_queues.py
+++ queues/implement_stack_using_queues.py
@@ -16,6 +16,29 @@ Notes:
     You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
     Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
 """
+# easier approach
+"""
+top - we go through every element in the queue except the last and and pop from the left while adding them to the right. Ensures the elem we want to pop is now on front .
+"""
+class MyStack:
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+
 class MyStack:
     """
     Stack implementation using two queues.
